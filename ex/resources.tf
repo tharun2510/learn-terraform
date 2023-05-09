e50d9bffc64eea6dd87b3f3df1ab5c5f39caee99
tarun <guntuputharunkumar95@gmail.com>
2023-05-10 02:08:58 +0530

    we have added no

---
+++ ex/resources.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "frontend" {
+  ami           = data.aws_ami.devops_practice_ami.image_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids  = [data.aws_security_group.test.id]
+  tags = {
+    Name = "frontend"
+
+  }
+}
+
+
+
+data "aws_ami" "devops_practice_ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = [973714476881]
+}
+
+
+data "aws_security_group" "test" {
+  name   = "allow-all"
+}
