c6027c5fac0bca38397b268fb614a5e6a1af220f
tarun <guntuputharunkumar95@gmail.com>
2023-05-09 19:20:38 +0530

    we have added no

--- roboshop.tf
+++ roboshop.tf
@@ -8,7 +8,6 @@ resource "aws_instance" "frontend" {
   }
 }
 
-
 output "frontend_pulic_ip" {
   value=aws_instance.frontend.public_ip
 }
@@ -18,7 +17,6 @@ data "aws_ami" "devops_practice_ami" {
   name_regex  = "Centos-8-DevOps-Practice"
   owners      = [973714476881]
 }
-
 
 output  "frontend_ami" {
   value = data.aws_ami.devops_practice_ami.image_id
