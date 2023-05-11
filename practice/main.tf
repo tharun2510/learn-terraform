304e539ad344f6e638939cec762dba64e144124e
tarun <guntuputharunkumar95@gmail.com>
2023-05-12 02:59:47 +0530

    we have added no

--- practice/main.tf
+++ practice/main.tf
@@ -1,13 +1,44 @@
 variable "components" {
   default = {
     frontend = {
-      name = "frontend"
+      name          = "frontend"
+      instance_type = "t3.micro"
     }
-    backend = {
-      name = "backend"
+    mongodb = {
+      name          = "mongodb"
+      instance_type = "t3.micro"
     }
-    middleware = {
-      name = "middleware"
+    catalogue = {
+      name          = "catalogue"
+      instance_type = "t3.micro"
+    }
+    redis = {
+      name          = "redis"
+      instance_type = "t3.micro"
+    }
+    user = {
+      name          = "user"
+      instance_type = "t3.micro"
+    }
+    mysql = {
+      name          = "mysql"
+      instance_type = "t3.micro"
+    }
+    cart = {
+      name          = "cart"
+      instance_type = "t3.micro"
+    }
+    shipping = {
+      name          = "shipping"
+      instance_type = "t3.micro"
+    }
+    rabbitmq = {
+      name          = "rabbitmq"
+      instance_type = "t3.micro"
+    }
+    payment = {
+      name          = "payment"
+      instance_type = "t3.micro"
     }
   }
 }
@@ -17,12 +48,14 @@ variable "components" {
 resource "aws_instance" "instances" {
   for_each=var.components
   ami           = data.aws_ami.example.image_id
-  instance_type = "t3.micro"
-
+  instance_type = each.value["instance_type"]
+  vpc_security_group_ids= [ data.aws_security_group.sg.id]
   tags = {
     Name = each.key
   }
 }
+
+aws_instance.instances["frontend"].public_ip
 
 
   data "aws_ami" "example" {
@@ -33,3 +66,16 @@ resource "aws_instance" "instances" {
     }
   }
 
+data "aws_security_group" "sg" {
+  name   = "allow-all"
+}
+
+
+resource "aws_route53_record" "domain_name" {
+  for_each = var.components
+  zone_id = "Z0542401RTZBLNQX3LCC"
+  name    = "${each.value["name"]}-dev.devops2510.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instances[each.value["name"]].public_ip]
+}
